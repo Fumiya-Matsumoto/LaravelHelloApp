fb92228d1be9da4756c865eee5b64bcf04276453
Fumiya-Matsumoto <ekiden238@gmail.com>
2022-07-31 02:37:24 +0900

    complete task

--- src/app/Repositories/TodoRepository.php
+++ src/app/Repositories/TodoRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 
 use App\Models\Todo;
 use App\Repositories\BaseRepository;
+use Illuminate\Support\Facades\Auth;
 
 /**
  * Class TodoRepository
@@ -39,4 +40,16 @@ class TodoRepository extends BaseRepository
     {
         return Todo::class;
     }
+    
+    public function search($sort)
+    {
+        $query = Todo::where('user_id', Auth::id());
+        if ($sort === 'titleAsc') $query->orderBy('title', 'asc');
+        if ($sort === 'titleDesc') $query->orderBy('title', 'desc');
+        if ($sort === 'statusAsc') $query->orderBy('status', 'asc');
+        if ($sort === 'statusDesc') $query->orderBy('status', 'desc');
+        $todos = $query->get();
+        return $todos;
+    }
+
 }
